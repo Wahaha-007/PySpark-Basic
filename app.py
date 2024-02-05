710b249bfeebee060337d9da7ea91bf3cb6073df
mana.m <mana.m@example.com>
2024-02-05 23:13:16 +0700

    Add Spark API for df

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 from util import get_spark_session
 from read import from_files
+from process import transform
 
 def main():
+  # --- 1. Read Data
   env = os.environ.get('ENVIRON')
   spark = get_spark_session(env,'GitHub Activity - Getting Started')
   src_dir = os.environ.get('SRC_DIR')
@@ -10,14 +12,17 @@ def main():
   src_file_format = os.environ.get('SRC_FILE_FORMAT')
 
   df = from_files(spark,src_dir,src_file_pattern,src_file_format)
-  print(env)
-  print(type(spark))
-  df.printSchema()
-  df.select('repo.*').show()
-
-  # Perform Spark operations here...
+  # print(env)
   # print(type(spark))
+  # df.printSchema()
+  # df.select('repo.*').show()
   # park.sql('SELECT current_date').show()
+
+  # --- 2. Tranform (Add column) data
+  df_transformed = transform(df)
+  df_transformed.printSchema()
+  df_transformed.select('repo.*','created_at','year','month','day').show()
+
 
   # Stop the Spark session when done
   spark.stop()
